d7bee49aa084cef52a5fbc927766337b3e4abf06
yohei <yohei_green@cloud.com>
2021-08-04 01:06:34 +0900

    hogre

--- discordbot.py
+++ discordbot.py
@@ -1,40 +1,166 @@
+import discord
+import requests
 from discord.ext import commands
+from pprint import pprint
+import aiohttp
 import os
 import traceback
-import discord
 
-bot = commands.Bot(command_prefix='/')
+bt = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
+@bt.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+
+TOKEN = "AAAAA"
+
+headers = {
+    "Authorization": "Bot AAAAA"
+}
 
 
-@bot.command()
-async def nya(ctx):
-    await ctx.send('nya')
+def returnNormalUrl(channelId):
+    return "https://discordapp.com/api/channels/" + str(channelId) + "/messages"
 
-@bot.command()
-async def wan(ctx):
-    await ctx.send('wan')
 
-@bot.command()
-async def drink(ctx):
-    embed=discord.Embed(title='DRINKS')
-    embed.add_field(name='ビール', value=':beer:')
-    embed.add_field(name='ワイン', value=':wine_glass:')
-    embed.add_field(name='ウイスキー', value=':whisky:')
-    embed.add_field(name='その他', value=':cocktail:')
-    embed.add_field(name='ジュース', value=':tropical_drink:')
-    await ctx.send(embed=embed)
+async def notify_callback(id, token):
+    url = "https://discord.com/api/v8/interactions/{0}/{1}/callback".format(id, token)
+    json = {
+        "type": 6
+    }
+    async with aiohttp.ClientSession() as s:
+        async with s.post(url, json=json) as r:
+            if 200 <= r.status < 300:
+                return
 
-bot.run(token)
+
+def normalMessage(msg, content):
+    normal_url = returnNormalUrl(msg["d"]["channel_id"])
+    json = {
+        "content": content
+    }
+    r = requests.post(normal_url, headers=headers, json=json)
+
+
+async def on_socket_response(msg):
+    if msg["t"] != "INTERACTION_CREATE":
+        return
+
+    pprint(msg)
+    custom_id = msg["d"]["data"]["custom_id"]
+
+    if custom_id == "click_one":
+        normal_url2 = returnNormalUrl(msg["d"]["channel_id"]) + "/" + msg["d"]["message"]["id"]
+        json2 = {
+            "content": "LEFT",
+            "components": [
+                {
+                    "type": 1,
+                    "components": [
+                        {
+                            "type": 2,
+                            "label": "<",
+                            "style": 1,
+                            "custom_id": "click_one",
+                            "disabled": True
+                        },
+                        {
+                            "type": 2,
+                            "label": ">",
+                            "style": 3,
+                            "custom_id": "click_two",
+                            "disabled": False
+                        },
+                    ]
+                }
+            ]
+        }
+        r2 = requests.patch(normal_url2, headers=headers, json=json2)
+        pprint(r2)
+        await notify_callback(msg["d"]["id"], msg["d"]["token"])
+    elif custom_id == 'click_two':
+        normal_url2 = returnNormalUrl(msg["d"]["channel_id"]) + "/" + msg["d"]["message"]["id"]
+        json2 = {
+            "content": "Right",
+            "components": [
+                {
+                    "type": 1,
+                    "components": [
+                        {
+                            "type": 2,
+                            "label": "<",
+                            "style": 1,
+                            "custom_id": "click_one",
+                            "disabled": False
+                        },
+                        {
+                            "type": 2,
+                            "label": ">",
+                            "style": 3,
+                            "custom_id": "click_two",
+                            "disabled": True
+                        },
+                    ]
+
+                }
+            ]
+        }
+        r2 = requests.patch(normal_url2, headers=headers, json=json2)
+        pprint(r2)
+        await notify_callback(msg["d"]["id"], msg["d"]["token"])
+
+
+class MyBot(commands.Bot):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_listener(on_socket_response)
+
+
+bot = MyBot(command_prefix='$', description='slash test')
+
+
+@bot.event
+async def on_ready():
+    print("Boot")
+    await bot.change_presence(activity=discord.Game("プロセカ"))
+
+
+@bot.event
+async def on_message(msg):
+    if msg.content == "hello":
+        normal_url = returnNormalUrl(msg.channel.id)
+        json = {
+            "content": "LEFT",
+            "components": [
+                {
+                    "type": 1,
+                    "components": [
+                        {
+                            "type": 2,
+                            "label": "<",
+                            "style": 1,
+                            "custom_id": "click_one",
+                            "disabled": True
+                        },
+                        {
+                            "type": 2,
+                            "label": ">",
+                            "style": 3,
+                            "custom_id": "click_two"
+                        },
+                    ]
+
+                }
+            ]
+        }
+        r = requests.post(normal_url, headers=headers, json=json)
+
+
+bot.run(TOKEN)
