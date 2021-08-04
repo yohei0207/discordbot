39be2f4247751cc9bbaa9b82b778430f640e3374
yohei <yohei_green@cloud.com>
2021-08-04 20:11:50 +0900

    hoge

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 token = os.environ['DISCORD_BOT_TOKEN']
 
+members = []
 num = 0
 
 
@@ -18,6 +19,14 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+@bot.event
+async def on_ready(ctx):
+    global members
+    all_members = ctx.channel.members
+    for member in all_members:
+        members.append(member)
+
 
 
 @bot.command()
@@ -33,7 +42,7 @@ async def nya(ctx):
 async def wan(ctx):
     await ctx.send('wan')
 
-"""
+
 @bot.command()
 async def drink(ctx):
     global num
@@ -47,15 +56,18 @@ async def drink(ctx):
     await ctx.send(embed=embed)
     await ctx.send(num)
 
-"""
+
 @bot.command()
 async def hito(ctx):
+    global members
+    """
     members = ctx.channel.members
     for member in members:
         await ctx.send(member.name)
-        #await ctx.send(member['discriminator'])
+        await ctx.send(member.discrimanator)
+    """
 
 
-    #await ctx.send(ctx.channel.members)
+    await ctx.send(members)
 
 bot.run(token)
